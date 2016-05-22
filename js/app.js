e4d778e697698b10e3fd7d6d1e6197d4d876342f
nikthequik <quik.the.nick@gmail.com>
2016-05-22 14:42:26 -0600

    make layout more resposive for mobile -title to absolute

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 $(function(){
-	$('.pull').hover(function() {
-		$(this).stop().animate({right: "25%"}, 500);
-		},
-		function() {
-		$(this).stop().animate({right: 0}, 500);
-		});
-
-	$('.push').hover(function() {
-		$(this).stop().animate({left: "25%"}, 500);
-		},
-		function() {
-		$(this).stop().animate({left: 0}, 500);
-		});
+	if ($(window).width() > 800) {
+		$('.pull').hover(function() {
+				$(this).stop().animate({right: "25%"}, 500);
+				},
+				function() {
+				$(this).stop().animate({right: 0}, 500);
+				});
+		$('.push').hover(function() {
+			$(this).stop().animate({left: "25%"}, 500);
+			},
+			function() {
+			$(this).stop().animate({left: 0}, 500);
+			});
+	}
 });
