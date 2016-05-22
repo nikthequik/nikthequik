08796b4bacf4721904a93b508808b04a0fb1b0cf
nikthequik <quik.the.nick@gmail.com>
2016-05-22 07:24:27 -0600

    -add alternating left and right sliding functionality

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 $(function(){
 	$('.pull').hover(function() {
-		$('.pull').stop().animate({right: "25%"}, 500);
-	},
-	function() {
-		$('.pull').stop().animate({right: 0}, 500);
-	});
+		$(this).stop().animate({right: "25%"}, 500);
+		},
+		function() {
+		$(this).stop().animate({right: 0}, 500);
+		});
+
+	$('.push').hover(function() {
+		$(this).stop().animate({left: "25%"}, 500);
+		},
+		function() {
+		$(this).stop().animate({left: 0}, 500);
+		});
 });
