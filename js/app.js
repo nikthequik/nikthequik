0f39e97ee5d314199a0f226c700dc6f9a1bd4018
nikthequik <quik.the.nick@gmail.com>
2016-05-23 10:56:28 -0600

    -adjust code so the article in each section will slide if the entire sectionis hovered over, and not just the article itself

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 $(function(){
 	if ($(window).width() > 866) {
 		$('.pull').hover(function() {
-				$(this).stop().animate({right: "25%"}, 500);
-				},
-				function() {
-				$(this).stop().animate({right: 0}, 500);
-				});
-		$('.push').hover(function() {
-			$(this).stop().animate({left: "25%"}, 500);
+			$(this).children('article').stop().animate({right: "25%"}, 500);
 			},
 			function() {
-			$(this).stop().animate({left: 0}, 500);
+			$(this).children('article').stop().animate({right: 0}, 500);
+			});
+		$('.push').hover(function() {
+			$(this).children('article').stop().animate({left: "25%"}, 500);
+			},
+			function() {
+			$(this).children('article').stop().animate({left: 0}, 500);
 			});
 	}
 });
