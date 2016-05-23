b33ab008eea2a7d4c1016caca364da99b2006d5d
nikthequik <quik.the.nick@gmail.com>
2016-05-23 14:14:35 -0600

    -add fadeIn functionality to hidden michigan picture in story section

--- js/app.js
+++ js/app.js
@@ -2,9 +2,13 @@ $(function(){
 	if ($(window).width() > 866) {
 		$('.pull').hover(function() {
 			$(this).children('article').stop().animate({right: "25%"}, 500);
+			
+				$('#michigan').stop().fadeIn(600);
+				
 			},
 			function() {
 			$(this).children('article').stop().animate({right: 0}, 500);
+			$('#michigan').stop().fadeOut("fast");
 			});
 		$('.push').hover(function() {
 			$(this).children('article').stop().animate({left: "25%"}, 500);
@@ -14,3 +18,8 @@ $(function(){
 			});
 	}
 });
+
+function reveal() {
+
+
+};
