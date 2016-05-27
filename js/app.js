4dfa2072aaff0deede246aa86fd8a4d5ca069c5d
nikthequik <quik.the.nick@gmail.com>
2016-05-26 18:20:52 -0600

    -resize and position pictures in story -border radius on quiz-pic

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
 $(function(){
 	//Perform only on screens above 842px
 	if ($(window).width() > 842) {
-		//Slide pull to left
-		$('.pull').hover(function() {
-			$(this).children('article').stop().animate({right: "25%"}, 500);
-			$(this).children('img').stop().fadeIn(1000);	
+		
+		$('#story').hover(function() {
+			$('#michigan, #nmu').stop().fadeIn(1000);
+			
 			},
 			function() {
-			$(this).children('article').stop().animate({right: 0}, 500);
-			$(this).children('img').stop().fadeOut("fast");
+			$('#michigan, #nmu').stop().fadeOut("fast");
 			});
 		//On hover make image appear
 		$('.push').hover(function() {
